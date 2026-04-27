976321246124f262c97082b8ba7ed9eb1e8adff7
Rudransh <rudranshupadhyay2004@gmail.com>
2026-04-27 23:21:28 +0530

    updated backend0

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: 'https://talksy01.vercel.app',
     },
 });
 
@@ -46,6 +46,8 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-server.listen(4600, () => {
-    console.log('server running at http://localhost:4600');
+const PORT = process.env.PORT || 4600;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
